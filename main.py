00075d572e4b1dc99fd069645aa9f6a2680dca1a
mdmeherabhossin860-blip <mdmeherabhossin860@gmail.com>
2026-02-20 05:31:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 class BossGuardApp(App):
     def build(self):
-        return Label(text='Boss Mehrab, Your App is Success!')
+        layout = BoxLayout(padding=10)
+        btn = Button(text="Success Boss Mehrab!\nYour App is Ready", 
+                     font_size='20sp',
+                     background_color=(0, 1, 0, 1))
+        layout.add_widget(btn)
+        return layout
 
 if __name__ == '__main__':
     BossGuardApp().run()
