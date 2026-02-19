189af146a5a4a9760c0d38171336b44aea64f09d
mdmeherabhossin860-blip <mdmeherabhossin860@gmail.com>
2026-02-19 20:59:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from kivy.uix.label import Label
 
 class BossGuardApp(App):
     def build(self):
-        # কোনো বাংলা অক্ষর রাখা যাবে না, আপাতত এটি দিয়ে টেস্ট করুন
-        return Label(text='Hello Boss Mehrab, Success!')
+        return Label(text='Boss Mehrab, Your App is Success!')
 
 if __name__ == '__main__':
     BossGuardApp().run()
