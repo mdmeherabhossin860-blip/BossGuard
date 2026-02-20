96fc7ee27a7ba4c5812813d0e7a88776592e2140
mdmeherabhossin860-blip <mdmeherabhossin860@gmail.com>
2026-02-20 23:36:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.core.window import Window
 
 class BossGuardApp(App):
     def build(self):
-        # ব্যাকগ্রাউন্ড কালার বা সুন্দর টেক্সট
-        Window.clearcolor = (0.1, 0.1, 0.1, 1) # ডার্ক থিম
-        return Label(
-            text='Welcome to BossGuard\nCreated by Mehrab Sir',
-            halign='center',
-            font_size='25sp',
-            color=(0, 1, 0, 1) # সবুজ রঙের লেখা
-        )
+        return Label(text='Boss Guard: Success!\nCreated by Mehrab Sir')
 
 if __name__ == '__main__':
     BossGuardApp().run()
