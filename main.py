9578adb752d3f7ec6fb204836dad426884db9259
mdmeherabhossin860-blip <mdmeherabhossin860@gmail.com>
2026-02-20 08:17:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import kivy
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
-class BossGuardApp(App):
+class PowerApp(App):
     def build(self):
-        return Label(text='Boss Mehrab, Your APK is Ready!')
+        layout = BoxLayout()
+        # এখানে আপনার জন্য একটি বিশেষ মেসেজ থাকবে
+        msg = Label(text="Mission Successful!\nMehrab Sir, Your APK is Ready.", 
+                    halign='center', font_size='20sp')
+        layout.add_widget(msg)
+        return layout
 
 if __name__ == '__main__':
-    BossGuardApp().run()
+    PowerApp().run()
